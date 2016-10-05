85ca0e89ff172f2108706d3c58fc62ca942079aa
macmade <macmade@xs-labs.com>
2016-10-05 02:04:11 +0200

    Icons for ASL senders...

--- Console/Classes/ASL/ASLSender.m
+++ Console/Classes/ASL/ASLSender.m
@@ -54,6 +54,51 @@
         self.facility        = facility;
         self.messagesMutable = [ NSMutableArray new ];
         self.messages        = @[];
+        
+        {
+            NSString                      * apps;
+            NSString                      * userApps;
+            NSString                      * path;
+            NSMutableArray<  NSString * > * paths;
+            
+            paths    = [ NSMutableArray new ];
+            apps     = NSSearchPathForDirectoriesInDomains( NSApplicationDirectory, NSLocalDomainMask, YES ).firstObject;
+            userApps = NSSearchPathForDirectoriesInDomains( NSApplicationDirectory, NSUserDomainMask,  YES ).firstObject;
+            
+            if( apps )
+            {
+                [ paths addObject: [ apps stringByAppendingPathComponent: [ NSString stringWithFormat: @"%@.app", self.name ] ] ];
+            }
+            
+            if( userApps )
+            {
+                [ paths addObject: [ userApps stringByAppendingPathComponent: [ NSString stringWithFormat: @"%@.app", self.name ] ] ];
+            }
+            
+            [ paths addObject: [ NSString stringWithFormat: @"/bin/%@",               self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/sbin/%@",              self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/usr/bin/%@",           self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/usr/sbin/%@",          self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/usr/libexec/%@",       self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/usr/local/bin/%@",     self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/usr/local/sbin/%@",    self.name ] ];
+            [ paths addObject: [ NSString stringWithFormat: @"/usr/local/libexec/%@", self.name ] ];
+            
+            for( path in paths )
+            {
+                if( [ [ NSFileManager defaultManager ] fileExistsAtPath: path ] )
+                {
+                    self.icon = [ [ NSWorkspace sharedWorkspace ] iconForFile: path ];
+                    
+                    break;
+                }
+            }
+            
+            if( self.icon == nil )
+            {
+                self.icon = [ [ NSWorkspace sharedWorkspace ] iconForFile: @"/bin/ls" ];
+            }
+        }
     }
     
     return self;
